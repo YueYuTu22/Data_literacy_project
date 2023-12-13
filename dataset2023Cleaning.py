d62b5dadc855a62f34a1a8a03e916a81c86886c2
MilesofEchos <136977546+YueYuTu22@users.noreply.github.com>
2023-12-13 11:10:26 +0100

    Add files via upload

--- dataset2023Cleaning.py
+++ dataset2023Cleaning.py
@@ -8,7 +8,7 @@ Created on Wed Nov 22 13:11:03 2023
 import pandas as pd
 
 # Replace 'your_file.csv' with the path to your CSV file
-file_path = r'C:\Semester3YYYY\DataLiteracy\archive\anime-dataset-2023.csv'
+file_path = r'archive\anime-dataset-2023.csv'
 
 # Read the CSV file into a DataFrame
 df = pd.read_csv(file_path)
@@ -18,13 +18,13 @@ df = df.dropna()
 column_name = 'Image URL'
 df = df.drop(column_name, axis=1)
 
-column_name2 = 'Licensors'
+column_name2 = 'Status'
 df = df.drop(column_name2, axis=1)
 
-column_name3 = 'Producers'
+column_name3 = 'Other name'
 df = df.drop(column_name3, axis=1)
 
-column_name4 = 'Studios'
+column_name4 = 'Aired'
 df = df.drop(column_name4, axis=1)
 
 column_name5 = 'anime_id'
@@ -36,22 +36,36 @@ df = df.drop(column_name6, axis=1)
 column_name7 = 'Premiered'
 df = df.drop(column_name7, axis=1)
 
+column_name8 = 'Source'
+df = df.drop(column_name8, axis=1)
+
+column_name8 = 'Duration'
+df = df.drop(column_name8, axis=1)
+
+column_name9 = 'Rating'
+df = df.drop(column_name9, axis=1)
+
 value_to_drop = 'UNKNOWN'
 
-mask = df['English name'] != value_to_drop
-df = df[mask]
+#mask = df['English name'] != value_to_drop
+#df = df[mask]
 
-mask2 = df['Rating'] != value_to_drop
-df = df[mask2]
+#mask2 = df['Producers'] != value_to_drop
+#df = df[mask2]
 
-mask3 = df['Genres'] != value_to_drop
-df = df[mask3]
+#mask3 = df['Genres'] != value_to_drop
+#df = df[mask3]
 
-df_head = df.head(10)
+df_replaced = df.replace('UNKNOWN', pd.NA)
+df = df_replaced.dropna()
 
-# Display the first 10 rows of the DataFrame
-print(df_head)
+#df_head = df.head(5)
+#column_names = df.columns
+#print(column_names)
+#print(df_head)
 
-column_names = df.columns
-print(column_names)
+output = r'archive\cleaned_dataset2023.csv' # This is a path in the current writable directory
+
+# Save the cleaned DataFrame to a CSV file
+df.to_csv(output, index=False)
 
---
+++ dataset2023_Classifier.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 13 10:53:11 2023
+
+@author: yuyue
+"""
+
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelEncoder
+
+file_path = r'archive\cleaned_dataset2023.csv'
+
+df = pd.read_csv(file_path)
+
+X = df[['Score', 'Rank', 'Popularity', 'Favorites', 'Scored By', 'Members']]
+y = df['Type']
+
+# Encoding the target variable 'Type' as it is categorical
+le = LabelEncoder()
+y_encoded = le.fit_transform(y)
+
+# Splitting the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.2, random_state=42)
+
+# Creating the Decision Tree classifier
+dt_classifier = DecisionTreeClassifier(random_state=42)
+
+# Fitting the classifier to the training data
+dt_classifier.fit(X_train, y_train)
+
+# Predicting on the test data
+y_pred = dt_classifier.predict(X_test)
+
+# Calculating the accuracy of the classifier
+accuracy = accuracy_score(y_test, y_pred)
+
+# Encoding the target variable 'Type' as it is categorical
+le = LabelEncoder()
+y_encoded = le.fit_transform(y)
+
+# Splitting the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.2, random_state=42)
+
+# Creating the Decision Tree classifier
+dt_classifier = DecisionTreeClassifier(random_state=42)
+
+# Fitting the classifier to the training data
+dt_classifier.fit(X_train, y_train)
+
+# Predicting on the test data
+y_pred = dt_classifier.predict(X_test)
+
+# Calculating the accuracy of the classifier
+accuracy = accuracy_score(y_test, y_pred)
+
+print(f'Accuracy: {accuracy}')
