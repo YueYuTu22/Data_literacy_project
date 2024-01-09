020ba057412e3221f524a04b71f86295e0f9241b
MilesofEchos <136977546+YueYuTu22@users.noreply.github.com>
2024-01-09 20:24:35 +0100

    Add files via upload

--- dataset2023Cleaning.py
+++ dataset2023Cleaning.py
@@ -4,10 +4,12 @@ Created on Wed Nov 22 13:11:03 2023
 
 @author: yuyue
 """
-
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Replace 'your_file.csv' with the path to your CSV file
+
 file_path = r'archive\anime-dataset-2023.csv'
 
 # Read the CSV file into a DataFrame
@@ -33,8 +35,8 @@ df = df.drop(column_name5, axis=1)
 column_name6 = 'Name'
 df = df.drop(column_name6, axis=1)
 
-column_name7 = 'Premiered'
-df = df.drop(column_name7, axis=1)
+#column_name7 = 'Premiered'
+#df = df.drop(column_name7, axis=1)
 
 column_name8 = 'Source'
 df = df.drop(column_name8, axis=1)
@@ -44,6 +46,17 @@ df = df.drop(column_name8, axis=1)
 
 column_name9 = 'Rating'
 df = df.drop(column_name9, axis=1)
+
+column_name10 = 'Synopsis'
+df = df.drop(column_name10, axis=1)
+
+column_name11 = 'Episodes'
+df = df.drop(column_name11, axis=1)
+
+column_name12 = 'Licensors'
+df = df.drop(column_name12, axis=1)
+
+
 
 value_to_drop = 'UNKNOWN'
 
@@ -59,13 +72,49 @@ value_to_drop = 'UNKNOWN'
 df_replaced = df.replace('UNKNOWN', pd.NA)
 df = df_replaced.dropna()
 
-#df_head = df.head(5)
-#column_names = df.columns
-#print(column_names)
-#print(df_head)
+df['Rank'] = pd.to_numeric(df['Rank'], errors='coerce')
 
+df['Score'] = pd.to_numeric(df['Score'], errors='coerce')
+df['Scored By'] = pd.to_numeric(df['Scored By'], errors='coerce')
+
+df['Weighted_Score'] = df['Score'] * df['Scored By']
+
+df.drop(['Score', 'Scored By'], axis=1, inplace=True)
+
+df['Weighted_Score'] = np.log1p(df['Weighted_Score'])
+
+
+df['Weighted_Score'] = np.interp(df['Weighted_Score'], (df['Weighted_Score'].min(), df['Weighted_Score'].max()), (1, 100))
+df = df.reset_index(drop=True)
 output = r'archive\cleaned_dataset2023.csv' # This is a path in the current writable directory
 
+studio_counts = df['Studios'].value_counts()
 # Save the cleaned DataFrame to a CSV file
 df.to_csv(output, index=False)
 
+def extract_year(premiered):
+    if premiered == 'UNKNOWN':
+        return None
+    else:
+        return int(premiered.split()[1])
+
+# Apply the function to extract the season and year from the "Premiered" column
+premiered_Year = df['Premiered'].map(extract_year)
+
+df['Premiered'] = premiered_Year
+
+# Filter out anime titles with popularity value 0
+df_valid_popularity = df[df['Popularity'] > 0]
+
+selected_studio = 'Wit Studio'  # Replace with the desired studio name
+studio_data = df[df['Studios'] == selected_studio]
+
+release_pattern = studio_data.groupby('Premiered')['English name'].count()
+
+plt.figure(figsize=(10, 6))
+plt.plot(release_pattern.index, release_pattern.values, marker='o', linestyle='-', color='b')
+plt.title(f'Release Pattern of Animes for {selected_studio}')
+plt.xlabel('Year')
+plt.ylabel('Number of Animes Released')
+plt.grid(True)
+plt.show()
