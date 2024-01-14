138ceb64a73e7f2d69dc53fc558cada874dcc72e
MilesofEchos <136977546+YueYuTu22@users.noreply.github.com>
2024-01-14 18:33:50 +0100

    Add files via upload

--- dataset2023_Classifier.py
+++ dataset2023_Classifier.py
@@ -7,41 +7,37 @@ Created on Wed Dec 13 10:53:11 2023
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
 from sklearn.metrics import classification_report
+from sklearn.preprocessing import StandardScaler
 
 #loading the data
 df = pd.read_csv('archive/cleaned_dataset2023.csv')
 
-type_counts = df['Type'].value_counts()
-Special_count = type_counts.get('Special', 0)
-TV_count = type_counts.get('TV', 0)
-Movie_count = type_counts.get('Movie', 0)
-ONA_count = type_counts.get('ONA', 0)
-ONA_count = type_counts.get('OVA', 0)
-
 #target column
-X = df[['Rank', 'Popularity', 'Favorites', 'Weighted_Score', 'Members']] 
+X = df[['Rank', 'Popularity', 'Favorites', 'Weighted_Score', 'Members']]
 y = df['Type']
 
-#divide dataset
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Standardize features by removing the mean and scaling to unit variance
+# SVMs assume that the data it works with is in a standard range
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
 
-#build logistic regression model
-model = LogisticRegression(
-    multi_class='multinomial', 
-    solver='newton-cg', 
-    C=0.01,  #Regularization
-    max_iter=500,  #Max iteration
-    tol=0.0001   #Tolerance
-)
+#divide dataset
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
+
+#build SVM model
+# Using the radial basis function (RBF) kernel
+model = SVC(kernel='rbf', gamma='scale', C=100)
 
 #train the model
 model.fit(X_train, y_train)
 
 #prediction
 predictions = model.predict(X_test)
-#waring! The line search algorithm did not converge
 
-result = classification_report(y_test, predictions)
+#evaluation
+result = classification_report(y_test, predictions, zero_division=0)
+
+print(result)
 
